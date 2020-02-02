ff1de01d60081775f08418ed219648a78a11f748
Lelberto <jsurieux@gmail.com>
2020-02-02 22:14:31 +0100

    Add errors tracing

--- src/services/express-service.ts
+++ src/services/express-service.ts
@@ -99,7 +99,7 @@ export default class ExpressService extends Service {
         app.use(mung.json((body, req, res) => {
             this.container.log.info(`${req.ip} > Requested ${req.method} ${req.originalUrl} in ${Date.now() - res.locals.data.start} ms`, { type: 'endpoints' });
             this.container.log.info(body, { type: 'endpoints' });
-        }));
+        }, { mungError: true }));
 
         // Registering controllers
         this.container.controllers.registerControllers(app);
