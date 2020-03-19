8b69b75b5e41451b40b136c7efbbe24590519f42
Lelberto <jsurieux@gmail.com>
2020-03-19 17:01:53 +0100

    Add PKCE system

--- src/services/oauth-service.ts
+++ src/services/oauth-service.ts
@@ -24,16 +24,26 @@ export default class OAuthService extends Service {
      * @param clientId Client ID
      * @param scope Scope
      * @param redirectUri Redirect URI
+     * @param codeChallenge Code challenge for PKCE
+     * @param codeChallenge Code challenge method for PKCE
      * @returns Generated ans encoded authorization code
      * @async
      */
-    public async generateAuthorizationCode(clientId: string, scope: string[], redirectUri: string): Promise<string> {
-        const code = await this.container.tokens.encode<AuthorizationCodeData>({
+    public async generateAuthorizationCode(clientId: string, scope: string[], redirectUri: string, codeChallenge?: string, codeChallengeMethod?: CodeChallengeMethod): Promise<string> {
+        if (codeChallenge != null) {
+            return await this.container.tokens.encode<AuthorizationCodeData>({
+                clientId,
+                scope,
+                redirectUri,
+                codeChallenge,
+                codeChallengeMethod,
+            }, process.env.AUTHORIZATION_CODE_SECRET, { expiresIn: parseInt(process.env.AUTHORIZATION_CODE_EXPIRATION, 10) });
+        }
+        return await this.container.tokens.encode<AuthorizationCodeData>({
             clientId,
             scope,
             redirectUri
         }, process.env.AUTHORIZATION_CODE_SECRET, { expiresIn: parseInt(process.env.AUTHORIZATION_CODE_EXPIRATION, 10) });
-        return code;
     }
 
     /**
@@ -45,13 +55,56 @@ export default class OAuthService extends Service {
      * @returns true if the authorization code is valid, false otherwise
      * @async
      */
-    public async verifyAuthorizationCode(code: string, clientId: string, redirectUri: string): Promise<boolean> {
+    public async verifyAuthorizationCode(code: string, clientId: string, redirectUri: string, codeVerifier?: string): Promise<boolean> {
         try {
             const authCode = await this.container.tokens.decode<AuthorizationCodeData>(code, process.env.AUTHORIZATION_CODE_SECRET);
+            if (authCode.codeChallenge != null) {
+                return (authCode.clientId === clientId) && (authCode.redirectUri === redirectUri) && this.verifyCodeChallenge(codeVerifier, authCode.codeChallenge, authCode.codeChallengeMethod);
+            }
             return (authCode.clientId === clientId) && (authCode.redirectUri === redirectUri);
         } catch (err) {
             return false;
         }
+    }
+
+    /**
+     * Verifies an authorization code.
+     * 
+     * @param codeVerifier Code verifier
+     * @param codeChallenge Code challenge
+     * @param codeChallengeMethod Code challenge method (algorithm)
+     * @returns true if the code verifier is valid, false otherwise
+     */
+    public verifyCodeChallenge(codeVerifier: string, codeChallenge: string, codeChallengeMethod: CodeChallengeMethod): boolean {
+        let encryptedCodeVerifier = null;
+        switch (codeChallengeMethod) {
+            default:
+            case 'plain':
+                encryptedCodeVerifier = codeVerifier;
+                break;
+            case 'S256':
+                encryptedCodeVerifier = this.container.crypto.encrypt(this.container.crypto.encrypt(codeVerifier, 'sha256'), 'base64');
+                break;
+            case 'S512':
+                encryptedCodeVerifier = this.container.crypto.encrypt(this.container.crypto.encrypt(codeVerifier, 'sha512'), 'base64');
+                break;
+        }
+        return encryptedCodeVerifier === codeChallenge;
+    }
+
+    /**
+     * Verifies if a code verifier is valid.
+     * 
+     * To be valid, the code verifier must respect the following rules :
+     * - Length between 43 and 128 characters
+     * - Alphabetical and numerical characters (A-Z a-z 0-9)
+     * - Punctuation characters (-._~)
+     * 
+     * @param codeVerifier Code verifier to verify
+     * @returns True if the code verifier is valid, false otherwise
+     */
+    public verifyCodeVerifier(codeVerifier: string): boolean {
+        return /[a-zA-Z\d-._~]{43,128}/g.test(codeVerifier);
     }
 }
 
@@ -59,3 +112,8 @@ export default class OAuthService extends Service {
  * Grant types type.
  */
 export type GrantType = 'authorization_code' | 'password' | 'client_credentials' | 'refresh_token';
+
+/**
+ * Code challenge methods.
+ */
+export type CodeChallengeMethod = 'plain' | 'S256' | 'S512';
