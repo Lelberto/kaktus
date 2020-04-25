af0828dd5ce36971b0613c43cc9998c3e3f4d8ac
Lelberto <jsurieux@gmail.com>
2020-04-25 20:28:38 +0200

    Add comments in the refresh token model

--- src/models/refresh-token-model.ts
+++ src/models/refresh-token-model.ts
@@ -1,20 +1,31 @@
-// TODO Add doc comments
 import { Document, Model, Mongoose, Schema } from 'mongoose';
 import ServiceContainer from '../services/service-container';
 import Attributes from './model';
 import { UserInstance } from './user-model';
 
+/**
+ * Refresh token attributes interface.
+ */
 export interface RefreshTokenAttributes extends Attributes {
     token: string;
     user: UserInstance;
 }
 
+/**
+ * Refresh token instance interface.
+ */
 export interface RefreshTokenInstance extends RefreshTokenAttributes, Document {}
 
 export default function createModel(container: ServiceContainer, mongoose: Mongoose): Model<RefreshTokenInstance> {
     return mongoose.model('RefreshToken', createRefreshTokenSchema(), 'refreshTokens');
 }
 
+/**
+ * Creates the refresh token model.
+ * 
+ * @param container Services container
+ * @param mongoose Mongoose instance
+ */
 function createRefreshTokenSchema() {
     const schema = new Schema({
         token: {
